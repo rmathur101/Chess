8a318110451087446cfe04bda8b256f4d2cdb667
Rohan Mathur <rmathur101@gmail.com>
2014-12-28 01:09:44 -0600

    knight tests passing

--- app/assets/javascripts/ng-app/constructors/knight.js
+++ app/assets/javascripts/ng-app/constructors/knight.js
@@ -1,2 +1,32 @@
 function Knight(){
+	this.getPossibles = function(squaresToPieces){
+		this.clearState();
+		this.squaresToPieces = squaresToPieces;
+		this.getCoordinates();
+		this.getPossiblePositions();
+		return this.possibles;
+	};
+
+	this.getPossiblePositions = function(){
+		this.addPossiblePosition(this.coordinates.x - 2, this.coordinates.y + 1);
+		this.addPossiblePosition(this.coordinates.x - 2, this.coordinates.y - 1);
+		this.addPossiblePosition(this.coordinates.x + 2, this.coordinates.y + 1);
+		this.addPossiblePosition(this.coordinates.x + 2, this.coordinates.y - 1);
+		this.addPossiblePosition(this.coordinates.x - 1, this.coordinates.y + 2);
+		this.addPossiblePosition(this.coordinates.x - 1, this.coordinates.y - 2);
+		this.addPossiblePosition(this.coordinates.x + 1, this.coordinates.y + 2);
+		this.addPossiblePosition(this.coordinates.x + 1, this.coordinates.y - 2);
+	};
+
+	this.addPossiblePosition = function(x, y){
+		var possible = convertCoordinatesToNotation(x, y);
+		if (isOnBoard(x,y) && (this.squaresToPieces[possible] == '' || this.squaresToPieces[possible].color != this.color)){
+			this.possibles.push(possible);
+		};
+	};
+};
+
+function initializeKnightConstructor(){
+	Knight.prototype = new Piece();
+	Knight.constructor = Knight;
 };
---
+++ spec/javascripts/knight_spec.js
@@ -0,0 +1,44 @@
+describe("Knight", function() {
+	describe("#getPossiblesKnight", function() {
+		var knight, enemy, friend;
+
+		beforeEach(function() {
+		  initializeKnightConstructor();
+		  knight = new Knight;
+		  knight.color = 'black';
+		  knight.position = 'g7';
+		  knight.getCoordinates();
+		  friend = new Pawn;
+		  friend.color = 'black';
+		  enemy = new Pawn;
+		  enemy.color = 'white';
+		});
+
+		it("adds possible squares for knight if there are no pieces in them", function() {
+			knight.squaresToPieces = {'e8': '', 'e6': '', 'f5': '', 'h5': ''};
+			knight.getPossiblePositions();
+			expect(knight.possibles).toContain('e8');
+			expect(knight.possibles).toContain('e6');
+			expect(knight.possibles).toContain('f5');
+			expect(knight.possibles).toContain('h5');
+			expect(knight.possibles.length).toEqual(4);
+		});
+
+		it("adds possible squares for knight if there are enemy pieces in them", function() {
+			knight.squaresToPieces = {'e8': enemy, 'e6': enemy, 'f5': enemy, 'h5': enemy};
+			knight.getPossiblePositions();
+			expect(knight.possibles).toContain('e8');
+			expect(knight.possibles).toContain('e6');
+			expect(knight.possibles).toContain('f5');
+			expect(knight.possibles).toContain('h5');
+			expect(knight.possibles.length).toEqual(4);
+		});
+
+		it("does not add any possible squares for knight if there are friendly pieces in them ", function() {
+			knight.squaresToPieces = {'e8': friend, 'e6': friend, 'f5': friend, 'h5': friend};
+			knight.getPossiblePositions();
+			expect(knight.possibles.length).toEqual(0);
+		});
+	});
+
+});
